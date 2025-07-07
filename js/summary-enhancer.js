7b9046514a6b94409f330c6742315986b1b961d0
Jonathon Lacher <jonathon.lacher@gmail.com>
2025-07-07 11:27:46 -0500

    Rebuild site 2025-07-07

--- js/summary-enhancer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const summaries = document.querySelectorAll("details > summary");
-  summaries.forEach((summary) => {
-    // Check if the text isn't already there to prevent multiple appends on reloads/navigation
-    if (!summary.textContent.includes("(click to expand)")) {
-      summary.innerHTML += " (click to expand)";
-    }
-  });
-});
